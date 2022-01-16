f84c76976ceb6cf7b9bea922b5dceb8b779b0d40
Jason Pell <jason@pellcorp.com>
2022-01-16 16:15:08 +1100

    Ubuntu version

--- customhidsony.sh
+++ customhidsony.sh
@@ -1,28 +1,22 @@
 #!/bin/bash 
 mkdir module
-echo "Removing previous kernel source"
-rm -rf kernel*.src.rpm*
-echo "Downloading kernel source from official repo"
-NAME=kernel-$(uname -r | sed 's/.x86_64//').src.rpm
-wget https://kojipkgs.fedoraproject.org//packages/kernel/$(uname -r | sed 's/.x86_64//' | sed s/-/\ /g | awk -F " " '{print $1}')/$(uname -r | sed 's/.x86_64//' | sed s/-/\ /g | awk -F " " '{print $2}')/src/kernel-$(uname -r | sed 's/.x86_64//').src.rpm
-echo "Extracting kernel source"
-mkdir extracted
-cd extracted
-rpm2cpio ../kernel-$(uname -r | sed 's/.x86_64//').src.rpm | cpio -idmv
-mv linux-* ../
-cd ..
-rm -rf extracted
-tar xvf $(ls | grep 'linux-*')
-rm linux-*.tar.xz
-mv linux* linux.src
-echo "Extracting files for sony dualshock driver"
-cp linux.src/drivers/hid/*sony* ./module
-cp linux.src/drivers/hid/*ids* ./module
-rm -rf linux.src
+
+echo "Downloading the kernel source package"
+sudo apt-get -y install dpkg-dev
+
+pushd /tmp 
+apt-get source linux
+popd
+
+PACKAGE_VERSION="$(uname -r | sed 's/.x86_64//' | sed s/-/\ /g | awk -F " " '{print $1}')"
+
+cp /tmp/linux-$PACKAGE_VERSION/drivers/hid/{hid-ids.h,hid-sony.c} ./module
+
 echo "Patching driver"
 patch -u -b ./module/hid-sony.c -i cloneDualshock.patch
 mv ./module/hid-sony.c ./module/hid-sony-clone-fix.c
 rm ./module/*.c.orig
+
 echo "Creating files for building"
 cat > "./module/Makefile" << _EOF_
 obj-m = hid-sony-clone-fix.o
@@ -46,27 +40,27 @@ AUTOINSTALL="yes"
 _EOF_
 echo "Stopping inbuilt and new module"
 if dkms status | grep -q "^hid-sony"; then
-    [[ -n "$(lsmod | grep hid_sony)" ]] && rmmod hid-sony
-    [[ -n "$(lsmod | grep hid_sony)" ]] && rmmod hid-sony-clone-fix
-    rm -rf /usr/src/hid-sony*
+    [[ -n "$(lsmod | grep hid_sony)" ]] && sudo rmmod hid-sony
+    [[ -n "$(lsmod | grep hid_sony)" ]] && sudo rmmod hid-sony_clone_fix
+    sudo rm -rf /usr/src/hid-sony*
 fi
+
+VERSION="$(uname -r | sed 's/.x86_64//' | sed s/-/\ /g | awk -F " " '{print $1}')"
+
 echo "Copying the module to DKMS"
-cp -rf "$(pwd)/module" "/usr/src/hid-sony-clone-fix-$(uname -r | sed 's/.x86_64//' | sed s/-/\ /g | awk -F " " '{print $1}')"
-KERNEL=""
-if [[ "$__chroot" -eq 1 ]]; then
-    KERNEL="$(ls -1 /lib/modules | tail -n -1)"
-else
-    KERNEL="$(uname -r)"
-fi
+sudo cp -rf "$(pwd)/module" "/usr/src/hid-sony-clone-fix-$VERSION"
+
+KERNEL="$(uname -r)"
+
 echo "Compiling DKMS module"
-dkms install -m hid-sony-clone-fix -v "$(uname -r | sed 's/.x86_64//' | sed s/-/\ /g | awk -F " " '{print $1}')" -k $KERNEL
-rmmod hid-sony
-modprobe hid-sony-clone-fix
-if dkms status | grep -q "^hid-sony-clone-fix"; then
+sudo dkms install -m hid-sony-clone-fix -v "$VERSION" -k $KERNEL
+sudo rmmod hid-sony
+sudo modprobe hid-sony-clone-fix
+if dkms status | grep -q "^hid_sony_clone_fix"; then
     echo "The script can't detect if everything works, if works please add a blacklist file for the module hid-sony and now use hid-sony-clone-fix"
 else
-    echo "For this step, you'll need the root password, if you dont have it, please skip and create the file /usr/lib/modprobe.d/hid-sony-blacklist.conf with this content blacklist hid-sony"
-    su root -c "echo \"blacklist hid-sony\" > /usr/lib/modprobe.d/hid-sony-blacklist.conf"
+    echo "blacklist hid-sony" | sudo tee /etc/modprobe.d/hid-sony-blacklist.conf > /dev/null
 fi
+
 rm -rf module
-rm kernel*.rpm
+
