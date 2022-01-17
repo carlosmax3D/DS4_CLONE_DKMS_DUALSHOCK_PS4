b4689f3e58452a2fc21a93e06a04339424a48bca
Carlos Israel Orta Orta <carlos.orta@coreapps.mx>
2022-01-17 14:27:13 -0600

    Fedora/Ubuntu Compatibility

--- README.md
+++ README.md
@@ -7,8 +7,10 @@ I figured out that some chinese controllers failed at plug it in the USB connect
 
 If you have these error, change your USB cable and try again and if the problem persist you have to patch the kernel driver. 
 
-This script has been customised for Ubuntu, if you want to use it on another distro maybe you have to change some paths but It should works anyway because it uses DKMS.
+This script has been customized for Ubuntu and Fedora, if you want to use it on another distro maybe you have to change some paths but It should works anyway because it uses DKMS.
 
 And also you need a patch file for the code, you need all these files in the same folder and you have to run the script with admin rights.
 
 I hope this help someone
+
+Execute this with sudo or as root
--- customhidsony.sh
+++ customhidsony.sh
@@ -1,17 +1,46 @@
 #!/bin/bash 
+OS_VER=$(lsb_release -a)
+VERSION="$(uname -r | sed 's/.x86_64//' | sed s/-/\ /g | awk -F " " '{print $1}')"
+KERNEL=""
+if echo ${OS_VER^^} | grep -q "FEDORA"; then
+    OS_VER="Fedora"
+else
+    OS_VER="Ubuntu"
+fi
 mkdir module
 
-echo "Downloading the kernel source package"
-sudo apt-get -y install dpkg-dev
+if [ ${OS_VER} = "Fedora" ]; then
+	echo "Removing previous kernel source"
+	echo "Downloading the kernel source from official repo"
+	rm -rf kernel*.src.rpm*
+	NAME=kernel-$(uname -r | sed 's/.x86_64//').src.rpm
+	wget https://kojipkgs.fedoraproject.org//packages/kernel/$(uname -r | sed 's/.x86_64//' | sed s/-/\ /g | awk -F " " '{print $1}')/$(uname -r | sed 's/.x86_64//' | sed s/-/\ /g | awk -F " " '{print $2}')/src/kernel-$(uname -r | sed 's/.x86_64//').src.rpm
+	echo "Extracting kernel source"
+	mkdir extracted
+	cd extracted
+	rpm2cpio ../kernel-$(uname -r | sed 's/.x86_64//').src.rpm | cpio -idmv
+	mv linux-* ../
+	cd ..
+	rm -rf extracted
+	tar xvf $(ls | grep 'linux-*' | grep 'tar.xz')
+	rm linux-*
+	mv linux* linux.src
+	echo "Extracting files for sony dualshock driver"
+	cp linux.src/drivers/hid/*sony* ./module
+	cp linux.src/drivers/hid/*ids* ./module
+	rm -rf linux.src
+else
+	echo "Downloading the kernel source package"
+	sudo apt-get -y install dpkg-dev
 
-pushd /tmp 
-apt-get source linux
-popd
+	pushd /tmp 
+	apt-get source linux
+	popd
 
-PACKAGE_VERSION="$(uname -r | sed 's/.x86_64//' | sed s/-/\ /g | awk -F " " '{print $1}')"
+	PACKAGE_VERSION="$(uname -r | sed 's/.x86_64//' | sed s/-/\ /g | awk -F " " '{print $1}')"
 
-cp /tmp/linux-$PACKAGE_VERSION/drivers/hid/{hid-ids.h,hid-sony.c} ./module
-
+	cp /tmp/linux-$PACKAGE_VERSION/drivers/hid/{hid-ids.h,hid-sony.c} ./module
+fi
 echo "Patching driver"
 patch -u -b ./module/hid-sony.c -i cloneDualshock.patch
 mv ./module/hid-sony.c ./module/hid-sony-clone-fix.c
@@ -39,28 +68,54 @@ DEST_MODULE_LOCATION[0]="/updates/dkms"
 AUTOINSTALL="yes"
 _EOF_
 echo "Stopping inbuilt and new module"
-if dkms status | grep -q "^hid-sony"; then
-    [[ -n "$(lsmod | grep hid_sony)" ]] && sudo rmmod hid-sony
-    [[ -n "$(lsmod | grep hid_sony)" ]] && sudo rmmod hid-sony_clone_fix
-    sudo rm -rf /usr/src/hid-sony*
+
+if [ ${OS_VER} = "Fedora" ]; then
+	if dkms status | grep -q "^hid-sony"; then
+	    [[ -n "$(lsmod | grep hid_sony)" ]] && rmmod hid-sony
+	    [[ -n "$(lsmod | grep hid_sony)" ]] && rmmod hid-sony-clone-fix
+	    rm -rf /usr/src/hid-sony*
+	fi
+else
+	if dkms status | grep -q "^hid-sony"; then
+	    [[ -n "$(lsmod | grep hid_sony)" ]] && sudo rmmod hid-sony
+	    [[ -n "$(lsmod | grep hid_sony)" ]] && sudo rmmod hid-sony_clone_fix
+	    sudo rm -rf /usr/src/hid-sony*
+	fi
 fi
-
-VERSION="$(uname -r | sed 's/.x86_64//' | sed s/-/\ /g | awk -F " " '{print $1}')"
-
 echo "Copying the module to DKMS"
-sudo cp -rf "$(pwd)/module" "/usr/src/hid-sony-clone-fix-$VERSION"
+if [ ${OS_VER} = "Fedora" ]; then
+	cp -rf "$(pwd)/module" "/usr/src/hid-sony-clone-fix-$VERSION"
+	if [[ "$__chroot" -eq 1 ]]; then
+	    KERNEL="$(ls -1 /lib/modules | tail -n -1)"
+	else
+	    KERNEL="$(uname -r)"
+	fi
+else
+	sudo cp -rf "$(pwd)/module" "/usr/src/hid-sony-clone-fix-$VERSION"
 
-KERNEL="$(uname -r)"
+	KERNEL="$(uname -r)"
+fi
 
 echo "Compiling DKMS module"
-sudo dkms install -m hid-sony-clone-fix -v "$VERSION" -k $KERNEL
-sudo rmmod hid-sony
-sudo modprobe hid-sony-clone-fix
-if dkms status | grep -q "^hid_sony_clone_fix"; then
-    echo "The script can't detect if everything works, if works please add a blacklist file for the module hid-sony and now use hid-sony-clone-fix"
+if [ ${OS_VER} = "Fedora" ]; then
+	dkms install -m hid-sony-clone-fix -v "$VERSION" -k $KERNEL
+	rmmod hid-sony
+	modprobe hid-sony-clone-fix
+	if dkms status | grep -q "^hid-sony-clone-fix"; then
+	    echo "The script can't detect if everything works, if works please add a blacklist file for the module hid-sony and now use hid-sony-clone-fix"
+	else
+	    echo "For this step, you'll need the root password, if you dont have it, please skip and create the file /usr/lib/modprobe.d/hid-sony-blacklist.conf with this content blacklist hid-sony"
+	    su root -c "echo \"blacklist hid-sony\" > /usr/lib/modprobe.d/hid-sony-blacklist.conf"
+	fi
+	rm kernel*.rpm
 else
-    echo "blacklist hid-sony" | sudo tee /etc/modprobe.d/hid-sony-blacklist.conf > /dev/null
+	sudo dkms install -m hid-sony-clone-fix -v "$VERSION" -k $KERNEL
+	sudo rmmod hid-sony
+	sudo modprobe hid-sony-clone-fix
+	if dkms status | grep -q "^hid_sony_clone_fix"; then
+	    echo "The script can't detect if everything works, if works please add a blacklist file for the module hid-sony and now use hid-sony-clone-fix"
+	else
+	    echo "blacklist hid-sony" | sudo tee /etc/modprobe.d/hid-sony-blacklist.conf > /dev/null
+	fi
 fi
-
 rm -rf module
-
